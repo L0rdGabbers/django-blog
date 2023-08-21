2b0b4a3c241b6d0a5ac62e062fa79547bc5996db
L0rdGabbers <>
2023-08-21 19:29:20 +0000

    Updates allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['godjango-app.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['godjango-app-7ab60996ec8e.herokuapp.com', 'localhost']
 
 
 # Application definition
