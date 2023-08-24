a0647d2dc068ce956bb6047297dd6dbfaf7bf252
L0rdGabbers <>
2023-08-24 17:55:29 +0000

    Reorganises models.py file and installs summernote

--- blog/models.py
+++ blog/models.py
@@ -12,14 +12,14 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    featured_image = CloudinaryField('image', default='placeholder')
     likes = models.ManyToManyField(
         User, related_name='blogpost_like', blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
         ordering = ["-created_on"]
